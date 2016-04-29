4419b0a52b6738aed0a201074bd960bbdaefda62
Randy Oest <oest@amazingrando.com>
2016-04-29 00:16:04 -0400

    Working on nav

--- themes/custom/fate/js_src/nav.js
+++ themes/custom/fate/js_src/nav.js
@@ -6,96 +6,56 @@
  */
 
 /* globals jQuery, Drupal */
+'use strict';
 
 (function ($, Drupal) {
-  'use strict';
+  Drupal.behaviors.fateNav = {
+    attach: function (context) {
+      var menus = $('.pane-menu-tree', context),
+          headers = $('.pane-menu-tree .pane-title:not(.active-menu)');
 
-  /**
-   * Reset heights for elements in a row against the current window width.
-   *
-   * @param {object} options
-   *   Contains configuration for resetHeights.
-   * @param {string} options.rowClass
-   *   The class for row elements.
-   * @param {string} options.blockClass
-   *   The class for block elements.
-   * @param {int} options.breakpoint
-   *   The integer representing the width at which we should start resetting
-   *   heights. This should usually correspond to a real breakpoint in sass.
-   *
-   * @returns {null} Nothing.
-   */
-  // var resetHeights = function (options) {
-  //   var contentRows = $(options.rowSelector),
-  //       contentBlocks   = $(options.blockSelector),
-  //       flexbox     = $('html').hasClass('flexbox'),
-  //       width       = $(window).width();
-  //
-  //   // Reset heights.
-  //   contentBlocks.css('height', '');
-  //
-  //   // Set heights if the browser does not support flexbox and the window width
-  //   // is greater than the specified breakpoint.
-  //   if (!flexbox && width > options.breakpoint) {
-  //     contentRows.each(function () {
-  //       var holdingPen  = [],
-  //           tallest     = 0;
-  //
-  //       // Get the height of each .content-block.
-  //       $(this).find(options.blockSelector).each(function () {
-  //         var contentBlock  = $(this),
-  //             blockHeight   = contentBlock.height();
-  //
-  //         holdingPen.push(contentBlock);
-  //
-  //         if (blockHeight > tallest) {
-  //           tallest = blockHeight;
-  //         }
-  //       });
-  //
-  //       // Make each .content-block in the row the same height.
-  //       $.each(holdingPen, function () {
-  //         $(this).height(tallest);
-  //       });
-  //     });
-  //   }
-  // };
-  //
-  // // Create a behavior for handling content blocks.
-  Drupal.behaviors.medstatContentBlocks = {
-    attach: function () {
-      // var resizeTimer;
-      console.log('nav.js FIRED2');
+      /**
+       * Process menus so they are accordians.
+       * @param  {jQuery} menus [The menus we process.]
+       * @return null.
+       */
+      $(menus).each(function (){
+        var $this = $(this);
+        $this.children('.active-trail').addClass('active-menu');
 
-      // Set the resize handler only once per page.
-      // $('body').once('medstatContentBlocks', function () {
-      //   $(window).on('load resize', function () {
-      //     clearTimeout(resizeTimer);
-      //     resizeTimer = setTimeout(function () {
-      //       // Reset heights for content blocks.
-      //       resetHeights({
-      //         breakpoint: 768,
-      //         rowSelector: '.content-row',
-      //         blockSelector: '.content-block'
-      //       });
-      //
-      //       // Reset heights for footer sections.
-      //       resetHeights({
-      //         breakpoint: 768,
-      //         rowSelector: '.footer-nav-section-wrapper',
-      //         blockSelector: '.footer-nav-section'
-      //       });
-      //
-      //       // Reset heights for article page recommend reading section.
-      //       resetHeights({
-      //         breakpoint: 768,
-      //         rowSelector: '.recommended-articles-columns',
-      //         blockSelector: '.recommended-articles-column'
-      //       });
-      //     }, 250);
-      //   });
-      // });
+        if ($this.find('.active-trail').length > 0) {
+          $this.addClass('menu-open').find('.pane-title').addClass('active-menu');
+        } else {
+          $this.addClass('menu-closed');
+        }
+      });
+
+      /**
+       * Headers will toggle menus.
+       * @param  {jQuery} headers [The main headers for each menu.]
+       * @return null
+       */
+      $(headers).on('click',function () {
+        $(this).parents('.pane-menu-tree').toggleClass('menu-open menu-closed');
+      });
+
+      /**
+       * Add menu controls.
+       * @param  {jQuery} 'li.expanded' [All li > ul]
+       * @return {null}
+       */
+      $('li.expanded').each(function (){
+        if ($(this).hasClass('active-trail')) {
+          $(this).children('a').after('<span class="menu-control menu-open"></span>');
+        }
+        else {
+          $(this).children('a').after('<span class="menu-control menu-closed"></span>');
+        }
+        $(this).addClass('has-menu-control');
+      });
+      $('.menu-control').on('click', function (){
+        $(this).toggleClass('menu-closed menu-open');
+      });
     }
   };
-
 })(jQuery, Drupal);
