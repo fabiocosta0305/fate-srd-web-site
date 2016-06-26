bc21a578e4136ef164bd58151d479da6c9d660a1
Randy Oest <oest@amazingrando.com>
2016-06-26 13:55:25 -0400

    Scroll to .js

--- themes/custom/fate/js/scripts.js
+++ themes/custom/fate/js/scripts.js
@@ -65,3 +65,41 @@
     }
   };
 })(jQuery, Drupal);
+
+/**
+ * @file
+ * Scroll to script.
+ *
+ * @author Randy Oest
+ */
+
+/* globals jQuery, Drupal */
+'use strict';
+
+(function ($, Drupal) {
+  Drupal.behaviors.fateScrollTo = {
+    attach: function (context) {
+      var $root = $('html, body', context);
+      var offset = 75;
+
+      $('a[href*=#]:not([href=#])').click(function() {
+        var href = $.attr(this, 'href');
+
+        $root.animate({
+          scrollTop: $(href).offset().top - offset
+        }, function () {
+          window.location.hash = href;
+        });
+
+        return false;
+      });
+
+      var t = window.location.hash;
+      if (t) {
+        $root.animate({
+          scrollTop: $(t).offset().top - offset
+        });
+      }
+    }
+  };
+})(jQuery, Drupal);
