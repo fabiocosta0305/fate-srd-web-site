138f296161d326b98751cd1e9774e9edca1236a4
Randy Oest <oest@amazingrando.com>
2016-07-03 23:20:04 -0400

    Add rules section header

--- themes/custom/fate/templates/system/page.tpl.php
+++ themes/custom/fate/templates/system/page.tpl.php
@@ -90,6 +90,9 @@
         </div>
       <?php endif; ?>
 
+      <?php if (isset($section)): ?>
+        <p class="rules-section"><?php print $section; ?></p>
+      <?php endif; ?>
       <?php if ($title): ?>
         <h1 class="page-title">
           <?php print $title; ?>
